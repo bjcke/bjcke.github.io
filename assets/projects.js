f8860d6cfc97a8ef748141e9f7bae42510f7b66e
Ben Coake <coakeben@gmail.com>
2026-01-29 18:31:36 +0000

    Publish Site 1.0

---
+++ assets/projects.js
@@ -0,0 +1,42 @@
+const PROJECTS = [
+  {
+    title: "Bop or Flop? A Semi-Supervised Approach to Predicting Song Popularity via Spotify Audio Features",
+    url: "/work/bop_flop_1/index.html",
+    date: "2025-06-01",
+    type: "academic",
+    discipline: ["python", "ml"],
+    description: "Explores using a combination of ML methods (K-Means Clustering, Random Forest) to predict song popularity."
+  },
+  {
+    title: "Bop or Flop 2.0: Advanced ML Methods for Spotify Song Popularity Prediction",
+    url: "/work/bop_flop_2/index.html",
+    date: "2025-10-01",
+    type: "academic",
+    discipline: ["python","ml", "statistics"],
+    description: "Second iteration of the song popularity project, with a focus on Bayesian Statistics."
+  },
+  {
+    title: "Data Analyst Portfolio",
+    url: "/work/l4_analyst/index.html",
+    date: "2023-10-01",
+    type: "business",
+    discipline: ["gsheets","dashboards"],
+    description: "Portfolio for a Level 4 Data Analyst Apprenticeship, achieving a Distinction."
+  },
+  {
+    title: "Tournament Engine",
+    url: "https://docs.google.com/spreadsheets/d/1sXHTt2YxpofzPIDWHAxEqQsjtr_g6izr5cypilGRS6A/edit?gid=954915003#gid=954915003",
+    date: "2025-08-01",
+    type: "personal",
+    discipline: ["gsheets"],
+    description: "I was approached to create a fully automated table tennis tournament engine in Google Sheets."
+  },
+  {
+    title: "JSON Generator",
+    url: "https://docs.google.com/spreadsheets/d/1v_2U-OwhAOcu65zSfYfNqPQZvD4OZfNptObzhxq5wRg/edit?gid=0#gid=0",
+    date: "2025-07-01",
+    type: "personal",
+    discipline: ["gsheets"],
+    description: "Created a customisable JSON request generator for running API PUT calls."
+  }
+];
